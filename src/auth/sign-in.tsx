8aa93c3d4dffecd7a9af7609e6f1dacad5776233
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-06-22 18:06:09 +0500

    modifiyig tsconig

--- src/auth/sign-in.tsx
+++ src/auth/sign-in.tsx
@@ -1,5 +1,4 @@
 import { SignIn } from "@clerk/clerk-react";
-import React from "react";
 
 function SignInPage() {
   return (
--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 import { UserButton, useUser } from "@clerk/clerk-react";
 
 function Header() {
-  const { user, isSignedIn} = useUser();
+  const { isSignedIn } = useUser();
   return (
     <div className="flex p-3 px-5 justify-between shadow-md">
       <img src="/logo.svg" alt="logo" className="h-10 w-10" />
